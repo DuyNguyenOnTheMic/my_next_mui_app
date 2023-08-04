0472fd35e2294170f323373d6c561b73a652392a
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-08-04 09:43:09 +0700

    Update components folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Header from '@/@core/components/header/Header'
-import Sidebar from '@/@core/components/sidebar/Sidebar'
+import Header from '@/components/header/Header'
+import Sidebar from '@/components/sidebar/Sidebar'
 import { Metadata } from 'next'
 import styles from '../styles/page.module.css'
 import Dashboard from './dashboard/page'
