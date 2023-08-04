7ffe086862d7f5b4d18d47eadce272ca83b1c304
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-08-04 14:45:26 +0700

    Update check if session is null

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import Header from '@/components/header.component'
 import Login from '@/components/login.component'
 import Sidebar from '@/components/sidebar.component'
+import { authOptions } from '@/lib/auth'
 import { Metadata } from 'next'
+import { getServerSession } from 'next-auth'
+import { Fragment } from 'react'
 import styles from '../styles/page.module.css'
 import Dashboard from './dashboard/page'
 
@@ -9,12 +12,18 @@ export const metadata: Metadata = {
   title: 'Home page'
 }
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions)
+
   return (
     <main className={styles.main}>
       <Header />
-      <Sidebar />
-      <Dashboard />
+      {session && (
+        <Fragment>
+          <Sidebar />
+          <Dashboard />
+        </Fragment>
+      )}
       <Login />
     </main>
   )
