cafea87ce4675268d872700427c02b2c0576f769
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-08-04 12:09:39 +0700

    Update ternary operator for login component

--- src/components/login.component.tsx
+++ src/components/login.component.tsx
@@ -8,20 +8,16 @@ import { Fragment } from 'react'
 const Login = () => {
   const { data: session } = useSession()
 
-  if (session) {
-    return (
-      <Fragment>
-        <p>Signed in as {session.user?.email}</p>
-        <p>Welcome {session.user?.name}</p>
-        <Avatar alt={session.user?.name as string} src={session.user?.image as string} />
-        <Button variant={'contained'} color={'error'} onClick={() => signOut()}>
-          Sign out
-        </Button>
-      </Fragment>
-    )
-  }
-
-  return (
+  return session ? (
+    <Fragment>
+      <p>Signed in as {session.user?.email}</p>
+      <p>Welcome {session.user?.name}</p>
+      <Avatar alt={session.user?.name as string} src={session.user?.image as string} />
+      <Button variant={'contained'} color={'error'} onClick={() => signOut()}>
+        Sign out
+      </Button>
+    </Fragment>
+  ) : (
     <Fragment>
       <p>Please log in</p>
       <Button variant={'contained'} color={'success'} onClick={() => signIn()}>
