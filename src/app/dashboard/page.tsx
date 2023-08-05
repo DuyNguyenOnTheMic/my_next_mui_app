93e706d9cc42d9db988bb571eb71c85d16880a25
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-08-05 14:09:04 +0700

    Update demo grid layout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,5 @@
+import scss from '@/styles/Dashboard.module.scss'
+import { Box, Grid, Paper } from '@mui/material'
 import { Metadata } from 'next'
 
 export const metadata: Metadata = {
@@ -5,5 +7,22 @@ export const metadata: Metadata = {
 }
 
 export default function Dashboard() {
-  return <h1>Dashboard</h1>
+  return (
+    <Box>
+      <Grid container gap={2} className={scss.topCardsContainer}>
+        <Grid>
+          <Paper className={scss.dataCard}>xs=4</Paper>
+        </Grid>
+        <Grid>
+          <Paper className={scss.dataCard}>xs=4</Paper>
+        </Grid>
+        <Grid>
+          <Paper className={scss.dataCard}>xs=4</Paper>
+        </Grid>
+      </Grid>
+      <Grid marginY={2}>
+        <Paper className={scss.dataCard}>xs=12</Paper>
+      </Grid>
+    </Box>
+  )
 }
