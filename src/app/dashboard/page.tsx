ccdd023d33f341fcdb4548b322fea6e9f7c622b5
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-08-05 16:04:46 +0700

    Update demo grid layout in dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import scss from '@/styles/Dashboard.module.scss'
 import { Box, Grid, Paper } from '@mui/material'
 import { Metadata } from 'next'
@@ -6,21 +8,21 @@ export const metadata: Metadata = {
   title: 'Dashboard'
 }
 
-export default function Dashboard() {
+export default function BasicGrid() {
   return (
-    <Box>
-      <Grid container gap={2} className={scss.topCardsContainer}>
-        <Grid>
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={4}>
           <Paper className={scss.dataCard}>xs=4</Paper>
         </Grid>
-        <Grid>
+        <Grid item xs={12} md={4}>
           <Paper className={scss.dataCard}>xs=4</Paper>
         </Grid>
-        <Grid>
+        <Grid item xs={12} md={4}>
           <Paper className={scss.dataCard}>xs=4</Paper>
         </Grid>
       </Grid>
-      <Grid marginY={2}>
+      <Grid item xs={12} marginY={2}>
         <Paper className={scss.dataCard}>xs=12</Paper>
       </Grid>
     </Box>
