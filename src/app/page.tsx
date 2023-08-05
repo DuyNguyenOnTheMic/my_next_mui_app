bf487af86845bd7ed63e84177f9cc6d2ae40fc61
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-08-05 09:16:38 +0700

    Remove redundant css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import { authOptions } from '@/lib/auth'
 import { Metadata } from 'next'
 import { getServerSession } from 'next-auth'
 import { Fragment } from 'react'
-import styles from '../styles/page.module.css'
 import Dashboard from './dashboard/page'
 
 export const metadata: Metadata = {
@@ -16,7 +15,7 @@ export default async function Home() {
   const session = await getServerSession(authOptions)
 
   return (
-    <main className={styles.main}>
+    <main>
       <Header />
       {session && (
         <Fragment>
