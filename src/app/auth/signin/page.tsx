9e8e211df67166e470d32035ee16d25b16c2dd52
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-08-08 17:25:06 +0700

    Customize UI for sign in page

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,3 +1,15 @@
+import Login from '@/components/Login'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import { useSession } from 'next-auth/react'
+
 export default function SignIn() {
-  return <h1>Sign in</h1>
+  const { data: session } = useSession()
+
+  return (
+    <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+      <Typography variant='h2'>{session ? 'Thanh you for logging in' : 'Please log in'}</Typography>
+      <Login />
+    </Box>
+  )
 }
