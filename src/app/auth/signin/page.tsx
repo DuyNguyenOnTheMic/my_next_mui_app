f4bb1902f7cf6f310b0ff913a0dadbef0736904b
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-08-09 09:44:02 +0700

    Updat alert message of signin

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,16 +1,16 @@
 import Login from '@/components/Login'
 import { authOptions } from '@/lib/auth'
-import Box from '@mui/material/Box'
-import Typography from '@mui/material/Typography'
+import Alert from '@mui/material/Alert'
+import Stack from '@mui/material/Stack'
 import { getServerSession } from 'next-auth'
 
 export default async function SignIn() {
   const session = await getServerSession(authOptions)
 
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-      <Typography variant='h2'>{session ? 'Thank you for logging in' : 'Please log in'}</Typography>
+    <Stack sx={{ justifyContent: 'center', alignItems: 'center' }} spacing={2}>
+      <Alert severity='info'>{session ? 'Thank you for logging in' : 'Please log in'}</Alert>
       <Login session={session} />
-    </Box>
+    </Stack>
   )
 }
