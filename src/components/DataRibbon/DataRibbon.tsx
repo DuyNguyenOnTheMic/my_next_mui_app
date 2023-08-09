2247aad7aa1608ec5a9c0ebe111222215821cbf2
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-08-09 11:36:00 +0700

    Update grid layout for data ribbon

--- src/components/DataRibbon/DataRibbon.tsx
+++ src/components/DataRibbon/DataRibbon.tsx
@@ -1,34 +1,36 @@
+import Box from '@mui/material/Box'
 import Grid from '@mui/material/Grid'
 import DataCard from '../DataCard'
-import scss from './DataRibbon.module.scss'
 
 export default function DataRibbon() {
   return (
-    <Grid container gap={2} className={scss.dataRibbon}>
-      <Grid>
-        <DataCard
-          title={'Total Sales'}
-          value={'462'}
-          description={'The totals of all DataSoft products in the current financial year'}
-        />
+    <Box sx={{ flexGrow: 1, textAlign: 'center' }}>
+      <Grid container spacing={2}>
+        <Grid item xs>
+          <DataCard
+            title={'Total Sales'}
+            value={'462'}
+            description={'The totals of all DataSoft products in the current financial year'}
+          />
+        </Grid>
+        <Grid item xs>
+          <DataCard
+            title={'Total Value'}
+            value={'$25,732.53'}
+            description={'The total sales of the current financial year'}
+          />
+        </Grid>
+        <Grid item xs>
+          <DataCard
+            title={'Avg. Order Value'}
+            value={'$159.30'}
+            description={'The average order value for all sales this current financial year'}
+          />
+        </Grid>
+        <Grid item xs>
+          <DataCard title={'Conversion rate'} value={'0.61%'} description={'How many pitches become sales'} />
+        </Grid>
       </Grid>
-      <Grid>
-        <DataCard
-          title={'Total Value'}
-          value={'$25,732.53'}
-          description={'The total sales of the current financial year'}
-        />
-      </Grid>
-      <Grid>
-        <DataCard
-          title={'Avg. Order Value'}
-          value={'$159.30'}
-          description={'The average order value for all sales this current financial year'}
-        />
-      </Grid>
-      <Grid>
-        <DataCard title={'Conversion rate'} value={'0.61%'} description={'How many pitches become sales'} />
-      </Grid>
-    </Grid>
+    </Box>
   )
 }
