6d390737d0d2939f5a9153fa5c647f13bbc9809d
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-08-09 09:55:13 +0700

    Update layout for dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,23 +1,11 @@
-import { Box, Grid, Paper } from '@mui/material'
-import scss from './Dashboard.module.scss'
+import { Box } from '@mui/material'
 
 export default function Dashboard() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2}>
-        <Grid item xs={12} md={4}>
-          <Paper className={scss.dataCard}>xs=4</Paper>
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <Paper className={scss.dataCard}>xs=4</Paper>
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <Paper className={scss.dataCard}>xs=4</Paper>
-        </Grid>
-      </Grid>
-      <Grid item xs={12} marginY={2}>
-        <Paper className={scss.dataCard}>xs=12</Paper>
-      </Grid>
+      {/* Data Ribbon - Showing numerical stats */}
+      {/* TransactionsPerDay */}
+      {/* Transaction Doughnut Charts Bottom Row */}
     </Box>
   )
 }
