5ef85b8fcc173616e2fbd6d656432bd887607808
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-08-10 11:11:09 +0700

    Update adđ data chart

---
+++ src/components/Dashboard/DataChart/DataChart.tsx
@@ -0,0 +1,29 @@
+import { darkOptions } from '@/components/DataChart/Themes'
+import { months } from '@/helper/Util'
+import { Chart, ChartConfiguration, registerables } from 'chart.js'
+import { useEffect, useRef } from 'react'
+
+export default function DataChart(props: ChartConfiguration) {
+  const { data, options } = props
+  const chartRef = useRef<HTMLCanvasElement>(null)
+
+  const labels = months({ count: 7 })
+  useEffect(() => {
+    if (chartRef.current) {
+      const chart = new Chart(chartRef.current, {
+        ...props,
+        options: {
+          ...options,
+          ...darkOptions
+        }
+      })
+
+      return () => chart.destroy()
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data])
+
+  return <canvas ref={chartRef} />
+}
+
+Chart.register(...registerables)
