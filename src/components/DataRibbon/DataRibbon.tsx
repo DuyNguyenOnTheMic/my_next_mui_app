28a3bed1680df96d19ae9b532f953ca095aff945
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-08-10 09:49:08 +0700

    Update responsive value for grid MUI

--- src/components/DataRibbon/DataRibbon.tsx
+++ src/components/DataRibbon/DataRibbon.tsx
@@ -6,28 +6,28 @@ export default function DataRibbon() {
   return (
     <Box sx={{ flexGrow: 1, textAlign: 'center' }}>
       <Grid container spacing={2}>
-        <Grid item xs>
+        <Grid item xs={12} md>
           <DataCard
             title={'Total Sales'}
             value={'462'}
             description={'The totals of all DataSoft products in the current financial year'}
           />
         </Grid>
-        <Grid item xs>
+        <Grid item xs={12} md>
           <DataCard
             title={'Total Value'}
             value={'$25,732.53'}
             description={'The total sales of the current financial year'}
           />
         </Grid>
-        <Grid item xs>
+        <Grid item xs={12} md>
           <DataCard
             title={'Avg. Order Value'}
             value={'$159.30'}
             description={'The average order value for all sales this current financial year'}
           />
         </Grid>
-        <Grid item xs>
+        <Grid item xs={12} md>
           <DataCard title={'Conversion rate'} value={'0.61%'} description={'How many pitches become sales'} />
         </Grid>
       </Grid>
