bc0957cee825030037472e2bf51577be54e479bc
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-08-13 23:48:34 +0700

    Update set color scheme to html element

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -17,6 +17,7 @@ export default function Providers({ children }: { children: React.ReactNode }) {
     () => ({
       toggleColorMode: () => {
         const chosenMode = mode === 'light' ? 'dark' : 'light'
+        document.documentElement.style.setProperty('color-scheme', chosenMode)
         setMode(chosenMode)
         localStorage.setItem('mode', chosenMode)
       }
