71fd4aeca2e5bc741f083363a6d6a040fec62ec5
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-08-14 12:12:09 +0700

    Update dark and light mode for data chart

--- src/components/DataChart/DataChart.tsx
+++ src/components/DataChart/DataChart.tsx
@@ -1,30 +1,35 @@
+import { useTheme } from '@mui/material/styles'
 import { Chart, ChartConfiguration, registerables } from 'chart.js'
 import ChartDataLabels from 'chartjs-plugin-datalabels'
 import { useEffect, useRef } from 'react'
-import { darkOptions } from './Themes'
+import { darkOptions, lightOptions } from './Themes'
 
 Chart.register(...registerables, ChartDataLabels)
 
 export default function DataChart(props: ChartConfiguration) {
   const { data, options } = props
+  const theme = useTheme()
+  const themeOptions = theme.palette.mode === 'light' ? lightOptions : darkOptions
   const chartRef = useRef<HTMLCanvasElement>(null)
 
   useEffect(() => {
     if (chartRef.current) {
-      new Chart(chartRef.current, {
+      const chart = new Chart(chartRef.current, {
         ...props,
         options: {
           ...options,
-          ...darkOptions,
+          ...themeOptions,
           plugins: {
             ...options?.plugins,
-            ...darkOptions?.plugins
+            ...themeOptions?.plugins
           }
         }
       })
+
+      return () => chart.destroy()
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [data])
+  }, [data, themeOptions])
 
   return <canvas ref={chartRef} />
 }
