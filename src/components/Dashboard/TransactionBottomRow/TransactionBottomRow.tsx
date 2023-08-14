d16d9edff815689707d789f942061ae76ccff727
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-08-14 20:45:28 +0700

    Update chart options to doughnut chart

--- src/components/Dashboard/TransactionBottomRow/TransactionBottomRow.tsx
+++ src/components/Dashboard/TransactionBottomRow/TransactionBottomRow.tsx
@@ -2,7 +2,24 @@ import DataChart from '@/components/DataChart'
 import { doughnutChartData } from '@/components/mockData'
 import Grid from '@mui/material/Grid'
 import Paper from '@mui/material/Paper'
+import { ChartConfiguration } from 'chart.js'
 import scss from './TransactionBottomRow.module.scss'
+
+const chartOptions: ChartConfiguration['options'] = {
+  plugins: {
+    datalabels: {
+      backgroundColor: 'rgba(0, 0, 0)',
+      opacity: 0.8,
+      borderRadius: 4,
+      color: 'white',
+      font: {
+        weight: 'bold'
+      },
+      formatter: Math.round,
+      padding: 6
+    }
+  }
+}
 
 export default function TransactionBottomRow() {
   return (
@@ -10,25 +27,25 @@ export default function TransactionBottomRow() {
       <Grid>
         <Paper className={scss.dataCard}>
           <p>Transactions per user type</p>
-          <DataChart type={'doughnut'} data={doughnutChartData} />
+          <DataChart type={'doughnut'} options={chartOptions} data={doughnutChartData} />
         </Paper>
       </Grid>
       <Grid>
         <Paper className={scss.dataCard}>
           <p>Transactions per user type</p>
-          <DataChart type={'doughnut'} data={doughnutChartData} />
+          <DataChart type={'doughnut'} options={chartOptions} data={doughnutChartData} />
         </Paper>
       </Grid>
       <Grid>
         <Paper className={scss.dataCard}>
           <p>Transactions per user type</p>
-          <DataChart type={'doughnut'} data={doughnutChartData} />
+          <DataChart type={'doughnut'} options={chartOptions} data={doughnutChartData} />
         </Paper>
       </Grid>
       <Grid>
         <Paper className={scss.dataCard}>
           <p>Transactions per user type</p>
-          <DataChart type={'doughnut'} data={doughnutChartData} />
+          <DataChart type={'doughnut'} options={chartOptions} data={doughnutChartData} />
         </Paper>
       </Grid>
     </Grid>
