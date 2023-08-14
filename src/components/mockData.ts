7d94b61b35351cc8f6d3af6c5632b0664316dae3
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-08-14 20:11:30 +0700

    Update line chart and doughnut chart mock data

--- src/components/mockData.ts
+++ src/components/mockData.ts
@@ -1,26 +1,27 @@
 import * as Utils from '@/helpers/Utils'
 
+const LINE_DATA_COUNT = 12
+const LINE_NUMBER_CFG = { count: LINE_DATA_COUNT, min: 0, max: 120 }
 export const lineChartData = {
   labels: Utils.months({ count: 12 }),
   datasets: [
     {
       label: 'Transactions',
-      data: [65, 59, 80, 81, 56, 55, 60, 49, 112, 72, 52, 43],
-      fill: false,
-      borderColor: 'rgb(75, 192, 192)',
-      tension: 0.1
+      data: Utils.numbers(LINE_NUMBER_CFG),
+      borderColor: Utils.CHART_COLORS.blue,
+      backgroundColor: Utils.transparentize(Utils.CHART_COLORS.blue, 0.5)
     }
   ]
 }
 
-const DATA_COUNT = 5
-const NUMBER_CFG = { count: DATA_COUNT, min: 0, max: 100 }
+const DOUGHNUT_DATA_COUNT = 5
+const DOUGHNUT_NUMBER_CFG = { count: DOUGHNUT_DATA_COUNT, min: 0, max: 100 }
 export const doughnutChartData = {
   labels: ['Red', 'Orange', 'Yellow', 'Green', 'Blue'],
   datasets: [
     {
       label: 'Dataset 1',
-      data: Utils.numbers(NUMBER_CFG),
+      data: Utils.numbers(DOUGHNUT_NUMBER_CFG),
       backgroundColor: Object.values(Utils.CHART_COLORS)
     }
   ]
