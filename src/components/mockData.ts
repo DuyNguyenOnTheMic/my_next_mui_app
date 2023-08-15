096225202aa6ec95f6e5177965cb58a75a9b0640
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-08-15 10:42:43 +0700

    Update mock data for chart

--- src/components/mockData.ts
+++ src/components/mockData.ts
@@ -1,40 +1,34 @@
 import * as Utils from '@/helpers/Utils'
 
-export function getLineChartData() {
-  const LINE_DATA_COUNT = 12
-  const LINE_NUMBER_CFG = { count: LINE_DATA_COUNT, min: 0, max: 120 }
-
-  return {
-    labels: Utils.months({ count: 12 }),
-    datasets: [
-      {
-        label: 'Transactions 1',
-        data: Utils.numbers(LINE_NUMBER_CFG),
-        borderColor: Utils.CHART_COLORS.red,
-        backgroundColor: Utils.transparentize(Utils.CHART_COLORS.red, 0.5)
-      },
-      {
-        label: 'Transactions 2',
-        data: Utils.numbers(LINE_NUMBER_CFG),
-        borderColor: Utils.CHART_COLORS.blue,
-        backgroundColor: Utils.transparentize(Utils.CHART_COLORS.blue, 0.5)
-      }
-    ]
-  }
+const LINE_DATA_COUNT = 12
+const LINE_NUMBER_CFG = { count: LINE_DATA_COUNT, min: 0, max: 120 }
+export const lineChartData = {
+  labels: Utils.months({ count: 12 }),
+  datasets: [
+    {
+      label: 'Transactions 1',
+      data: Utils.numbers(LINE_NUMBER_CFG),
+      borderColor: Utils.CHART_COLORS.red,
+      backgroundColor: Utils.transparentize(Utils.CHART_COLORS.red, 0.5)
+    },
+    {
+      label: 'Transactions 2',
+      data: Utils.numbers(LINE_NUMBER_CFG),
+      borderColor: Utils.CHART_COLORS.blue,
+      backgroundColor: Utils.transparentize(Utils.CHART_COLORS.blue, 0.5)
+    }
+  ]
 }
 
-export function getDoughnutChartData() {
-  const DOUGHNUT_DATA_COUNT = 5
-  const DOUGHNUT_NUMBER_CFG = { count: DOUGHNUT_DATA_COUNT, min: 0, max: 100 }
-
-  return {
-    labels: ['Red', 'Orange', 'Yellow', 'Green', 'Blue'],
-    datasets: [
-      {
-        label: 'Dataset 1',
-        data: Utils.numbers(DOUGHNUT_NUMBER_CFG),
-        backgroundColor: Object.values(Utils.CHART_COLORS)
-      }
-    ]
-  }
+const DOUGHNUT_DATA_COUNT = 5
+const DOUGHNUT_NUMBER_CFG = { count: DOUGHNUT_DATA_COUNT, min: 0, max: 100 }
+export const doughnutChartData = {
+  labels: ['Red', 'Orange', 'Yellow', 'Green', 'Blue'],
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: Utils.numbers(DOUGHNUT_NUMBER_CFG),
+      backgroundColor: Object.values(Utils.CHART_COLORS)
+    }
+  ]
 }
