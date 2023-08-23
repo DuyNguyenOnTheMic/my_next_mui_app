77bb0c5508122fd024da0bf7259b71d32e35459e
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-08-23 10:01:00 +0700

    Add settings page to complete the app

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -1,3 +1,82 @@
+'use client'
+
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import FormControl from '@mui/material/FormControl'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import FormGroup from '@mui/material/FormGroup'
+import Grid from '@mui/material/Grid'
+import Switch from '@mui/material/Switch'
+import Typography from '@mui/material/Typography'
+import { Fragment, useState } from 'react'
+
 export default function Settings() {
-  return <h1>Settings</h1>
+  const [showRevenue, setShowRevenue] = useState(true)
+  const [showProfit, setShowProfit] = useState(true)
+  const [showOrders, setShowOrders] = useState(true)
+  const [showCustomers, setShowCustomers] = useState(true)
+
+  const handleShowRevenueChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setShowRevenue(event.target.checked)
+  }
+
+  const handleShowProfitChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setShowProfit(event.target.checked)
+  }
+
+  const handleShowOrdersChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setShowOrders(event.target.checked)
+  }
+
+  const handleShowCustomersChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setShowCustomers(event.target.checked)
+  }
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    // TODO: handle form submission
+  }
+
+  return (
+    <Fragment>
+      <h1>Settings</h1>
+      <Box>
+        <Typography variant='h4' gutterBottom>
+          Dashboard Features
+        </Typography>
+        <form onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <FormControl component='fieldset'>
+                <FormGroup>
+                  <FormControlLabel
+                    control={<Switch checked={showRevenue} onChange={handleShowRevenueChange} />}
+                    label='Revenue'
+                  />
+                  <FormControlLabel
+                    control={<Switch checked={showProfit} onChange={handleShowProfitChange} />}
+                    label='Profit'
+                  />
+                  <FormControlLabel
+                    control={<Switch checked={showOrders} onChange={handleShowOrdersChange} />}
+                    label='Orders'
+                  />
+                  <FormControlLabel
+                    control={<Switch checked={showCustomers} onChange={handleShowCustomersChange} />}
+                    label='Customers'
+                  />
+                </FormGroup>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+              <Button type='submit' variant='contained' color='primary'>
+                Save Settings
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </Box>
+    </Fragment>
+  )
 }
